6d88cd5657a4bf0f5bdeaf6c41de31e181be3dfa
Nicholas Crofts <nicholas.crofts@gmail.com>
2018-10-24 19:35:26 -0400

    crushin crud on movie model

--- movie_app/app/controllers/movies_controller.rb
+++ movie_app/app/controllers/movies_controller.rb
@@ -1,13 +1,43 @@
 class MoviesController < ApplicationController
-  def index
-  end
 
-  def new
+  def index
+    @movies = Movie.all
   end
 
   def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create  
+    @movie = Movie.create!(movie_params)
+    redirect_to movie_path(@movie)
   end
 
   def edit
+    @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+    @movie.update(movie_params)
+    redirect_to "/movies/#{@movie.id}"
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to "/movies"
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :genre, :year, :synopsis, :picture)
+  end
+
+
 end
--- movie_app/app/models/movie.rb
+++ movie_app/app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+    # include HTTParty
+    # base_uri 'http://www.omdbapi.com/?apikey=f3d1e28d'
 end
