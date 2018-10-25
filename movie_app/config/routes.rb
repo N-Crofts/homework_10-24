ca4b3d4e20f49d4403d63f0647d1cb6b7a497f49
Nicholas Crofts <nicholas.crofts@gmail.com>
2018-10-24 22:08:36 -0400

    add User model with devise ... very far from it actually doing anything

--- movie_app/config/routes.rb
+++ movie_app/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/new'
+  get 'user/show'
+  get 'user/edit'
+  devise_for :users
   root to: 'movies#index'
   resources :movies
 end
